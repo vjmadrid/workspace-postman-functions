ed7045a4e0d3498e267203c2ca8285e9f9062965
Rafael Hernamperez Martín <rhernamperez@atsistemas.com>
2023-05-11 16:30:06 +0200

    checkCollectionVarsSet function added

--- src/postman_global_functions2.js
+++ src/postman_global_functions2.js
@@ -13,6 +13,9 @@ globals = {
         return true
     },
 
+    /**
+     * Global operations 
+     */
     checkGlobalsVarsSet: function(variable_list){
         this.isParameterUndefined(variable_list, 'variable_list')
 
@@ -20,6 +23,23 @@ globals = {
             const value = pm.globals.get(variable);
             if(typeof(value) === "undefined"){
                 throw new Error("Please, go to 'Environments -> Globals' and set: ${variable_list.join(', ')}")
+            }
+        })
+
+        return true
+    },
+
+
+    /**
+     * Collection operations 
+     */
+    checkCollectionVarsSet: function(variable_list){
+        this.isParameterUndefined(variable_list, 'variable_list')
+
+        variable_list.forEach(variable => {
+            const value = pm.collectionVariables.get(variable);
+            if(typeof(value) === "undefined"){
+                throw new Error("Please, go to 'Collections (left panel) -> select collection -> Select Variables tab' and set: ${variable_list.join(', ')}")
             }
         })
 

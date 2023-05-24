47186b2472a0e33f8562c8d9378073053b324c86
Rafael Hernamperez Martín <rhernamperez@atsistemas.com>
2023-05-24 16:29:10 +0200

    Code normalized and definitive comments

--- src/postman_global_functions2.js
+++ src/postman_global_functions2.js
@@ -6,78 +6,94 @@ globals = {
     },
 
     isParameterUndefined: function(parameter, parameter_name){
-        if(typeof(parameter) === "undefined"){
+        if(typeof(parameter) === "undefined") {
             throw new Error(parameter_name + " is undefined")
         }
 
-        return true
+        return true;
     },
+
 
     /**
      * Global variables operations 
      */
+
+    // Given a list of variables (variable names in a string array), check if all of them are defined as global variables
     checkGlobalsVarsSet: function(variable_list){
-        this.isParameterUndefined(variable_list, 'variable_list')
+        this.isParameterUndefined(variable_list, 'variable_list');
 
         variable_list.forEach(variable => {
             const value = pm.globals.get(variable);
+
             if(typeof(value) === "undefined"){
                 throw new Error("Please, go to 'Environments -> Globals' and set: ${variable_list.join(', ')}")
             }
-        })
+        });
 
-        return true
+        return true;
     },
 
 
     /**
      * Environment variables operations 
      */
+
+    // Given a list of variables (variable names in a string array), check if all of them are defined as environment variables
     checkEnvironmentVarsSet: function(variable_list){
-        this.isParameterUndefined(variable_list, 'variable_list')
+        this.isParameterUndefined(variable_list, 'variable_list');
 
         variable_list.forEach(variable => {
             const value = pm.environment.get(variable);
-            if(typeof(value) === "undefined"){
+
+            if(typeof(value) === "undefined") {
                 throw new Error("Please, go to 'Enviroments (left panel) -> select enviroment' and set the variable '" + variable + "'");
             }
-        })
+        });
 
-        return true
+        return true;
     },
+
 
     /**
      * Collection variables operations 
      */
+
+    // Given a list of variables (variable names in a string array), check if all of them are defined as collection variables
     checkCollectionVarsSet: function(variable_list){
-        this.isParameterUndefined(variable_list, 'variable_list')
+        this.isParameterUndefined(variable_list, 'variable_list');
 
         variable_list.forEach(variable => {
             const value = pm.collectionVariables.get(variable);
-            if(typeof(value) === "undefined"){
+
+            if(typeof(value) === "undefined") {
                 throw new Error("Please, go to 'Collections (left panel) -> select collection -> Select Variables tab' and set the variable '" + variable + "'");
             }
-        })
+        });
 
-        return true
+        return true;
     },
+
 
     /**
      * Data variables operations 
-     * Data variables are defined in data files in JSON or CSV format (collection runner)
+     * - Note: Data variables are defined in data files in JSON or CSV format (collection runner)
      */
+
+    // Given a list of variables (variable names in a string array), check if all of them are defined as data variables
     checkDataVarsSet: function(datavar_list){
-        this.isParameterUndefined(datavar_list, 'datavar_list')
+        this.isParameterUndefined(datavar_list, 'datavar_list');
 
         datavar_list.forEach(datavar => {
             const value = pm.iterationData.get(datavar);
-            if(typeof(value) === "undefined"){
+
+            if(typeof(value) === "undefined") {
                 throw new Error("Please, go to 'Runner (Postman foot (right))' and load the data file containing the data variable '" + datavar + "'");
             }
-        })
+        });
 
-        return true
+        return true;
     },
+
 
     /**
      * Checking the list of headers for a request
@@ -87,12 +103,11 @@ globals = {
      *   By default, the header name is empty. We must to assign the name with the key if we want to access it
      */
     checkRequestHeaders: function(header_list) {
-        this.isParameterUndefined(header_list, 'header_list')
+        this.isParameterUndefined(header_list, 'header_list');
         const headers = pm.request.headers;
 
         //Print HeaderList
         //console.log(headers)
-        // To get count of headers 
         //console.log("Count of headers in request: "+headers.count())
         
         // Iterate one by one. Assign the name of each header with its key (name=key)
@@ -104,13 +119,15 @@ globals = {
 
         header_list.forEach(header => {
             const value = pm.request.headers.get(header);  // Get value by its header name
-            if(typeof(value) === "undefined"){
+
+            if(typeof(value) === "undefined") {
                 throw new Error("Please, set the header '" + header + "' explicitly");
             }
-        })
+        });
 
-        return true
+        return true;
     },
+
 
     /**
      * Checking the list of headers for a response
@@ -119,12 +136,11 @@ globals = {
      *   By default, the header name is empty. We must to assign the name with the key if we want to access it
      */
     checkResponseHeaders: function(header_list) {
-        this.isParameterUndefined(header_list, 'header_list')
+        this.isParameterUndefined(header_list, 'header_list');
         const headers = pm.response.headers;
 
         //Print HeaderList
         //console.log(headers)
-        // To get count of headers 
         //console.log("Count of headers in response: "+headers.count())
         
         // Iterate one by one. Assign the name of each header with its key (name=key)
@@ -136,59 +152,65 @@ globals = {
 
         header_list.forEach(header => {
             const value = pm.response.headers.get(header);  // Get value by its header name
-            if(typeof(value) === "undefined"){
+
+            if(typeof(value) === "undefined") {
                 throw new Error("Header '" + header + "' was not returned");
             }
-        })
+        });
 
-        return true
+        return true;
     },
 
+
     /**
-     * Checking a list of parameters in the path of request
+     * Checking a list of parameters defined in the path of a request
      * Example: http://www.mysite.com?param=value&param=value
      */
     checkRequestPathParams: function(param_list) {
-        this.isParameterUndefined(param_list, 'param_list')
+        this.isParameterUndefined(param_list, 'param_list');
 
         param_list.forEach(param => {
             const value = pm.request.url.query.get(param); 
-            if(typeof(value) === "undefined"){
+
+            if(typeof(value) === "undefined") {
                 throw new Error("Please, set the param '" + param + "' explicitly in the url path");
             }
-        })
+        });
 
-        return true
+        return true;
     },
 
+
     /**
-     * Checking a list of path variables in the path of request
+     * Checking a list of variables defined in the path of a request
      * Example: http://www.mysite.com/:var
      * 
      * IMPORTANT: In the testing, don't use ":" before the name of variable
      *    postman_global_functions.checkRequestPathVars(["id"]);
      */
     checkRequestPathVars: function(variable_list) {
-        this.isParameterUndefined(variable_list, 'variable_list')
+        this.isParameterUndefined(variable_list, 'variable_list');
 
         variable_list.forEach(variable => {
             const value = pm.request.url.variables.get(variable);
-            if(typeof(value) === "undefined"){
+
+            if(typeof(value) === "undefined") {
                 throw new Error("Please, set the variable ':" + variable + "' explicitly in the url path and assign it a value");
             }
-        })
+        });
 
-        return true
+        return true;
     },
+
 
     /**
      * Checking a list of keys in the Request Body (Json format)
      * IMPORTANT: 
-     *    - Internally, postman adds the keys 'mode' and 'raw'. Our JSON is children of 'raw'
+     *    - Internally, postman adds the keys 'mode' and 'raw'. Our JSON is children of 'raw' key
      *    - 'raw' key stores our JSON body as string. You must parse it to JSON Object
      */
     checkRequestBodyJson: function(key_list) {
-        this.isParameterUndefined(key_list, 'key_list')
+        this.isParameterUndefined(key_list, 'key_list');
 
         if (pm.request.body.isEmpty()) {
             throw new Error("Body is empty. Please, define a body in JSON format");
@@ -197,37 +219,39 @@ globals = {
         const jsonData = pm.request.body.toJSON();
 
         if (typeof(jsonData.raw) === undefined) {
-            throw new Error("Please, define your JSON data in the body request. Remember specify a 'raw' body and JSON format");            
+            throw new Error("Please, define your JSON data in the body request. Remember specify a 'raw' body and JSON format");
         }
 
         ourBody = JSON.parse(jsonData.raw);
 
         key_list.forEach(key => {
             value = ourBody[key];
-            if(typeof(value) === "undefined"){
+            if(typeof(value) === "undefined") {
                 throw new Error("Please, set the key '" + key + "' explicitly in the JSON data in the body request");
             }
-        })
+        });
 
-        return true
+        return true;
     },
+
 
     /**
      * Checking a list of keys in the Response Body (Json format)
      */
     checkResponseJson: function(key_list) {
-        this.isParameterUndefined(key_list, 'key_list')
+        this.isParameterUndefined(key_list, 'key_list');
         const jsonData = pm.response.json();
 
         key_list.forEach(key => {
             value = jsonData[key];
-            if(typeof(value) === "undefined"){
+            if(typeof(value) === "undefined") {
                 throw new Error("The key '" + key + "' is not present in the JSON data in the response");
             }
-        })
+        });
 
-        return true
+        return true;
     },
+
 
     /**
      * Checking a list of cookies
@@ -237,12 +261,12 @@ globals = {
 
         cookie_list.forEach(cookie => {
             const value = pm.cookies.get(cookie);
-            if(typeof(value) === "undefined"){
+            if(typeof(value) === "undefined") {
                 throw new Error("Please, set the cookie ':" + cookie + "'.");
             }
-        })
+        });
 
-        return true
+        return true;
     }
 
 }
